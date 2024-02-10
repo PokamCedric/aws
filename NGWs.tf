b65a198adc07bcc75708960d82d6759f39674920
Cedric Joel Ngameni Pokam <cngamenipokam@gmail.com>
2024-02-10 17:34:53 +0100

    terraform fmt

--- NGWs.tf
+++ NGWs.tf
@@ -1,9 +1,9 @@
 
 # Create NAT-Gateways
 resource "aws_nat_gateway" "nat_gw" {
-  count = length(var.public_web_subnets_cidr)
+  count         = length(var.public_web_subnets_cidr)
   allocation_id = element(aws_eip.eip[*].id, count.index)
-  subnet_id = element(aws_subnet.public-web-subnet[*].id, count.index)
+  subnet_id     = element(aws_subnet.public-web-subnet[*].id, count.index)
 
   # To ensure proper ordering, it is recommended to add an explicit dependency
   # on the Internet Gateway for the VPC.
