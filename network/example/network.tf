53ec88b4712488ef2c0e0b8915d5e601ac9aaa6b
Cedric Joel Ngameni Pokam <cngamenipokam@gmail.com>
2024-02-14 13:30:45 +0100

    reformate example

--- network/example/network.tf
+++ network/example/network.tf
@@ -5,11 +5,25 @@ module "network" {
   enable_dns_support   = true
   enable_dns_hostnames = true
 
-  azs_names       = data.aws_availability_zones.azs.names
-  private_subnets = [{ name = "app", cidrs = ["172.20.4.0/24", "172.20.5.0/24", "172.20.6.0/24"] }, { name = "db", cidrs = ["172.20.7.0/24", "172.20.8.0/24", "172.20.9.0/24"] }]
-  public_subnets  = [{ name = "web", cidrs = ["172.20.1.0/24", "172.20.2.0/24", "172.20.3.0/24"] }, { name = "jenkins", cidrs = ["172.20.10.0/24", "172.20.11.0/24", "172.20.12.0/24"] }]
+  azs_names       = slice(data.aws_availability_zones.azs.names, 0, 3)
+
+  public_subnets  = [
+          { name = "web",
+            cidrs = ["172.20.1.0/24", "172.20.2.0/24", "172.20.3.0/24"] }
+        ]
+
+  private_subnets = [
+          { name = "app",
+            cidrs = ["172.20.4.0/24", "172.20.5.0/24", "172.20.6.0/24"] },
+          { name = "db",
+            cidrs = ["172.20.7.0/24", "172.20.8.0/24", "172.20.9.0/24"] }
+        ]
 
   vpc_tags = {
     Name = "App-VPC"
   }
+}
+
+output "azs_names" {
+  value = module.network.azs_names
 }
