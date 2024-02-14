e9d536ea301b65732a62c9fb7567c84f5096e15e
Cedric Joel Ngameni Pokam <cngamenipokam@gmail.com>
2024-02-14 09:49:29 +0100

    refactore vpc

--- network/VPC.tf
+++ network/VPC.tf
@@ -1,9 +1,9 @@
 # Create a VPC
-resource "aws_vpc" "vpc" {
-  cidr_block           = var.vpc_cidr
-  enable_dns_support   = true
-  enable_dns_hostnames = true
-  tags = {
-    Name = "app-vpc"
-  }
+resource "aws_vpc" "this" {
+  cidr_block            = var.vpc_cidr
+  instance_tenancy      = var.instance_tenancy
+  enable_dns_hostnames  = var.enable_dns_hostnames
+  enable_dns_support    = var.enable_dns_support
+  tags =  var.vpc_tags
+
 }
