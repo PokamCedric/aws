250dbb22779b21e08f08f5ac37230c4c6d385038
Cedric Joel Ngameni Pokam <cngamenipokam@gmail.com>
2024-02-23 00:35:10 +0100

    Create network

--- complete/network.tf
+++ complete/network.tf
@@ -1,16 +1,17 @@
 module "network" {
   source = "../modules/network"
 
+  azs_count            = var.azs_count  # This is required
   vpc_cidr             = var.vpc_cidr
   enable_dns_support   = true
   enable_dns_hostnames = true
 
-  azs_names       = slice(data.aws_availability_zones.azs.names, 0, 2)
+  azs_names       = slice(data.aws_availability_zones.azs.names, 0, var.azs_count)
   public_subnets  = var.public_subnets
   private_subnets = var.private_subnets
-  vpc_tags = {
-    Name = "Dev-VPC"
-  }
+
+  vpc_tags  = { Name = "Dev-VPC" }
+  igw_tags  = { Name = "Dev-IGW" }
 }
 
 locals {
