8d1335a379b9900590f2144edb081ff891a23e8b
Cedric Joel Ngameni Pokam <cngamenipokam@gmail.com>
2024-03-11 21:54:27 +0100

    fixed s3 object mime type

--- modules/s3/folder_object/object.tf
+++ modules/s3/folder_object/object.tf
@@ -2,39 +2,35 @@
 # aws s3 cp personal-blog/src/_site/ s3://www.my-awesome-site.com/ --recursive
 
 ##### will upload all the files present under HTML folder to the S3 bucket #####
-resource "aws_s3_object" "upload_object" {
-  for_each     = fileset(var.file_source_path, "**")
-  bucket       = var.bucket
-  key          = each.key
-  source       = "${var.file_source_path}/${each.key}"
-  etag         = filemd5("${var.file_source_path}/${each.key}")
-  content_type = "text/html"
+
+locals {
+  default_mime_type = "text/plain"
+  mime_types = {
+    "css"  = "text/css"
+    "html" = "text/html"
+    "ico"  = "image/vnd.microsoft.icon"
+    "jpeg" = "image/jpeg"
+    "jpg"  = "image/jpg"
+    "js"   = "application/javascript"
+    "json" = "application/json"
+    "map"  = "application/json"
+    "png"  = "image/png"
+    "svg"  = "image/svg+xml"
+    "txt"  = "text/plain"
+  }
 }
 
-/*
-> fileset(path.module, "files/*.txt")
-[
-  "files/hello.txt",
-  "files/world.txt",
-]
+resource "aws_s3_object" "upload_object" {
+  for_each = fileset(var.file_source_path, "**")
 
-> fileset(path.module, "files/{hello,world}.txt")
-[
-  "files/hello.txt",
-  "files/world.txt",
-]
+  bucket = var.bucket
+  key    = each.key
+  source = "${var.file_source_path}/${each.key}"
+  etag   = filemd5("${var.file_source_path}/${each.key}")
 
-> fileset("${path.module}/files", "*")
-[
-  "hello.txt",
-  "world.txt",
-]
-
-> fileset("${path.module}/files", "**")
-[
-  "hello.txt",
-  "world.txt",
-  "subdirectory/anotherfile.txt",
-]
-
-*/
+  content_type = lookup(
+    tomap(local.mime_types),
+    element(split(".", each.key), length(split(".", each.key)) - 1),
+    local.default_mime_type
+  )
+}
--- modules/s3/single_object/object.tf
+++ modules/s3/single_object/object.tf
@@ -2,6 +2,5 @@ resource "aws_s3_object" "this" {
   bucket = var.bucket
   key    = var.file_destination_path
   source = var.file_source_path
-
-  etag = filemd5(var.file_source_path)
+  etag   = filemd5(var.file_source_path)
 }
