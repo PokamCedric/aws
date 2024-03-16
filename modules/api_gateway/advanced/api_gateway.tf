1fe7cd626a8a5edc97ccb1bdb09a13c50bdda338
Cedric Joel Ngameni Pokam <cngamenipokam@gmail.com>
2024-03-16 21:47:32 +0100

    Used  module for api_gateway

--- modules/api_gateway/advanced/api_gateway.tf
+++ modules/api_gateway/advanced/api_gateway.tf
@@ -1,6 +1,6 @@
 locals {
   rest_api_id = aws_api_gateway_rest_api.this.id
-  resource_id = local.resource_id
+  resource_id = aws_api_gateway_resource.this.id
   http_method = aws_api_gateway_method.this.http_method
 }
 
--- portfolio/backend/api_gateway.tf
+++ portfolio/backend/api_gateway.tf
@@ -1,79 +1,24 @@
 locals {
-  api_gateway_rest_api = "${aws_api_gateway_rest_api.visitorCounter.execution_arn}/*"
-  rest_api_id          = aws_api_gateway_rest_api.visitorCounter.id
-  resource_id          = aws_api_gateway_resource.cors_resource.id
-  http_method          = aws_api_gateway_method.increment_visitors_method.http_method
+  api_gateway_rest_api = "${module.aws_api_gateway.rest_api_execution_arn}/*"
+  rest_api_id          = module.aws_api_gateway.rest_api_id
+  resource_id          = module.aws_api_gateway.resource_id
   http_method_option   = aws_api_gateway_method.options_method.http_method
 }
 
-resource "aws_api_gateway_rest_api" "visitorCounter" {
-  name        = "visitorCounter"
-  description = "Visitor Counter API"
-  endpoint_configuration {
-    types = ["REGIONAL"]
-  }
+module "aws_api_gateway" {
+  source = "../../modules/api_gateway/advanced/"
+
+  resource        = var.resource
+  api_description = "Visitor Counter API"
+  method          = "POST"
+  stage_name      = "dev"
+  inegration_uri  = module.lambda.invoke_arn
+  #  type        = "AWS_PROXY"
+
 }
 
-resource "aws_api_gateway_resource" "cors_resource" {
-  path_part   = var.resource
-  parent_id   = aws_api_gateway_rest_api.visitorCounter.root_resource_id
-  rest_api_id = local.rest_api_id
-}
-
-resource "aws_api_gateway_method" "increment_visitors_method" {
-  rest_api_id   = local.rest_api_id
-  resource_id   = local.resource_id
-  http_method   = "POST"
-  authorization = "NONE"
-}
-
-resource "aws_api_gateway_method_response" "method_response_200" {
-  depends_on = [aws_api_gateway_method.increment_visitors_method]
-
-  rest_api_id = local.rest_api_id
-  resource_id = local.resource_id
-  http_method = local.http_method
-  status_code = "200"
-  response_parameters = {
-    "method.response.header.Access-Control-Allow-Origin" = true
-  }
-}
-
-resource "aws_api_gateway_integration" "lambda_visitor_integration" {
-  rest_api_id             = local.rest_api_id
-  resource_id             = local.resource_id
-  http_method             = local.http_method
-  integration_http_method = "POST"
-  type                    = "AWS"
-  uri                     = module.lambda.invoke_arn
-}
-
-resource "aws_api_gateway_integration_response" "VisitorIntegrationResponse" {
-  depends_on = [aws_api_gateway_integration.lambda_visitor_integration]
-
-  rest_api_id = local.rest_api_id
-  resource_id = local.resource_id
-  http_method = local.http_method
-  status_code = aws_api_gateway_method_response.method_response_200.status_code
-  response_parameters = {
-    "method.response.header.Access-Control-Allow-Origin" = "'*'"
-  }
-}
-
-resource "aws_api_gateway_deployment" "visitor_api_deployment" {
-  depends_on = [aws_api_gateway_integration.lambda_visitor_integration]
-
-  rest_api_id = local.rest_api_id
-  stage_name  = var.stage_name
-
-  triggers = {
-    "redeployment" = sha1(jsonencode([
-      aws_api_gateway_resource.cors_resource.id,
-      aws_api_gateway_method.increment_visitors_method.http_method,
-      aws_api_gateway_integration.lambda_visitor_integration.id,
-      aws_api_gateway_method_response.method_response_200.id
-    ]))
-  }
+output "url" {
+  value = "${module.aws_api_gateway.deployment_invoke_url}/${var.resource}"
 }
 
 # OPTIONS HTTP method.
@@ -132,8 +77,4 @@ resource "aws_api_gateway_integration_response" "options_integration_response" {
     "method.response.header.Access-Control-Allow-Methods" = "'OPTIONS,POST'",
     "method.response.header.Access-Control-Allow-Origin"  = "'*'"
   }
-}
-
-output "url" {
-  value = "${aws_api_gateway_deployment.visitor_api_deployment.invoke_url}/${var.resource}"
 }
